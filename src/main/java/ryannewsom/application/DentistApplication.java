774eb6eb7d9888dd66246131fa71cc3537691671
Ryan Newsom <Ryan.J.Newsom@kp.org>
2016-11-25 14:42:50 -0700

    changed seed again

--- src/main/java/ryannewsom/application/DentistApplication.java
+++ src/main/java/ryannewsom/application/DentistApplication.java
@@ -37,11 +37,11 @@ public class DentistApplication implements CommandLineRunner {
         }
     }
 
-    //Seeds Appointments for 1 year from today
+    //Seeds Appointments for 1 year from 11/24/2016
     private void seedAppointments() {
         final Office office = new Office(new ContactInfo("3034541287", MockOffices.OFFICE_1_ADDRESS));
         long ONE_HOUR_MS = 3600000;
-        final long START_TIME = System.currentTimeMillis();
+        final long START_TIME = 1479999600000l;
         long ONE_YEAR_MS;
         ONE_YEAR_MS = ONE_HOUR_MS * 24 * 365;
         long endTime = START_TIME + ONE_YEAR_MS;
