6e5f723168b459d1cf3ee105186072602c9ec02d
Ryan Newsom <Ryan.J.Newsom@kp.org>
2016-12-01 21:31:45 -0700

    ignore

--- src/main/java/ryannewsom/application/AppointmentCustomRepository.java
+++ src/main/java/ryannewsom/application/AppointmentCustomRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import ryannewsom.model.appointment.Appointment;
 
 /**
- * Provides Custom Mongo Specific Implementation
+ * Provides Custom Mongo Specific Implementation. 
  */
 @Repository
 public class AppointmentCustomRepository {
